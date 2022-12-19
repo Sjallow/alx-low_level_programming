a288c6be7644ba352e50acdce1816bbb67bf7ee9
Sjallow <seringj54@gmail.com>
2022-12-19 12:51:22 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
  * _strcpy  -  Copies a string pointed to by @src, including the
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
- * Return: A pointer to the destination string @dest.
+ * Return: Always 0
  */
 char *_strcpy(char *dest, const char *src)
 {
