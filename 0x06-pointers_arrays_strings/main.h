e209826184d79aba7be9c98759a182195bbcd82e
Sjallow <seringj54@gmail.com>
2022-12-21 09:40:00 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * File: holberton.h
+ * File: main.h
  * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  * used in the 0x05-pointers_arrays_strings directory.
