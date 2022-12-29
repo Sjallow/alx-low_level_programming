0a9fc4fc9740e16f5fafc64e0b50edca9616c1c2
Sjallow <seringj54@gmail.com>
2022-12-29 05:34:57 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,24 @@
  */
 int main(void)
 {
-
-	int d;
-
+int digit1, digit2;
 
 
-	for (d = 0; d < 100; d++)
-
-		{
-
-		putchar((d / 10) + '0');
-
-		putchar((d % 10) + '0');
-if (d != 99)
+for (digit1 = 0; digit1 < 9; digit1++)
 {
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
+if (digit1 == 8 && digit2 == 9)
+	continue;
 putchar(',');
-																putchar(' ');
+putchar(',');	
+}
 }
 
-}
+putchar('\n');
 
-					putchar('\n');
-
-					return (0);
+return (0);
 }
