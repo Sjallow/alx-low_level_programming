445c92b0d40f420bf845c312dd0a8ace2dac824d
Sjallow <seringj54@gmail.com>
2022-12-31 02:33:27 -0800

    8-24_hours

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ * time_table  - check time table
+ */
+void times_table(void)
+{
+	int i, j, k;
+	for (j = 0, j < 10, j++)
+	{
+		k = j * i;
+		if (j == 0)
+		{
+			_putchar(k + '0');
+		}
+
+		if (k <10 && j ! = 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
+		} else if (k >= 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+	}
+
